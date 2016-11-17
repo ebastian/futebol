cb7aa29e2320d866820c76e86191687a5cff856c
Eduardo <eduardo.bastian@gmail.com>
2016-11-17 13:33:56 -0200

    Iniciando desenvolvimento do formulário base

--- app/services/team.mock.js
+++ app/services/team.mock.js
@@ -4,26 +4,31 @@ exports.TEAMS = [
         id: 1,
         name: "Gabcube",
         description: "recontextualize best-of-breed models",
+        type: "team",
         players: "efernandez0"
     }, {
         id: 2,
         name: "Gigaclub",
         description: "cultivate B2B experiences",
+        type: "team",
         players: "lreed1"
     }, {
         id: 3,
         name: "Aibox",
         description: "deploy customized architectures",
+        type: "team",
         players: "jlittle2"
     }, {
         id: 4,
         name: "Gigabox",
         description: "drive compelling channels",
+        type: "team",
         players: "ethomas3"
     }, {
         id: 5,
         name: "Gabtype",
         description: "redefine open-source content",
+        type: "team",
         players: "eknight4"
     }
 ];
--- app/shared/registryform/form-screen.component.js
+++ app/shared/registryform/form-screen.component.js
@@ -9,9 +9,9 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require("@angular/core");
-var router_1 = require("@angular/router");
-var router_2 = require("@angular/router");
-var generic_service_1 = require("../../shared/service/generic-service");
+var router_1 = require('@angular/router');
+var router_2 = require('@angular/router');
+var generic_service_1 = require('../../shared/service/generic-service');
 var entity_1 = require("../entity/entity");
 var FormScreenComponent = (function () {
     function FormScreenComponent(route, router, service) {
@@ -54,26 +54,24 @@ var FormScreenComponent = (function () {
             console.log('Especify the listpath property');
         }
     };
+    __decorate([
+        core_1.Output(), 
+        __metadata('design:type', Object)
+    ], FormScreenComponent.prototype, "onSave", void 0);
+    __decorate([
+        core_1.Output(), 
+        __metadata('design:type', Object)
+    ], FormScreenComponent.prototype, "onDelete", void 0);
+    FormScreenComponent = __decorate([
+        core_1.Component({
+            template: '<h1>Form Screen</h1>',
+            providers: [
+                generic_service_1.GenericService
+            ]
+        }), 
+        __metadata('design:paramtypes', [router_1.ActivatedRoute, router_2.Router, generic_service_1.GenericService])
+    ], FormScreenComponent);
     return FormScreenComponent;
 }());
-__decorate([
-    core_1.Output(),
-    __metadata("design:type", Object)
-], FormScreenComponent.prototype, "onSave", void 0);
-__decorate([
-    core_1.Output(),
-    __metadata("design:type", Object)
-], FormScreenComponent.prototype, "onDelete", void 0);
-FormScreenComponent = __decorate([
-    core_1.Component({
-        template: '<h1>Form Screen</h1>',
-        providers: [
-            generic_service_1.GenericService
-        ]
-    }),
-    __metadata("design:paramtypes", [router_1.ActivatedRoute,
-        router_2.Router,
-        generic_service_1.GenericService])
-], FormScreenComponent);
 exports.FormScreenComponent = FormScreenComponent;
 //# sourceMappingURL=form-screen.component.js.map
